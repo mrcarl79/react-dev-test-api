83c5015021edd141d99029cd4b3a6fe517a3122a
Carl Southgate <carl@tella.co.nz>
2022-05-04 18:24:24 +1200

    refactor: updated test api

--- pages/api/test.ts
+++ pages/api/test.ts
@@ -1,15 +1,23 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 
 type Response = {
-    borrowing: number
+    error?: string
+    borrowing?: number
 }
 
-type NextApiRequestWithExtras = NextApiRequest & {
-    total_income: number
-    total_liabilities: number
-}
-
-export default function handler(req: NextApiRequestWithExtras, res: NextApiResponse<Response>) {
-    if (req.method === 'POST') res.status(200).json({ borrowing: req.total_income * 5 - req.total_liabilities })
-    else res.status(400).end(`Invalid Data`)
+export default function handler(req: NextApiRequest, res: NextApiResponse<Response>) {
+    try {
+        if (req.method === 'POST') {
+            if (!req.body) res.status(400).json({ error: 'missing request body' })
+            const body = JSON.parse(req.body)
+            if (!body.total_income) res.status(400).json({ error: 'missing property total_income' })
+            if (!body.total_liabilities) res.status(400).json({ error: 'missing property total_liabilities' })
+            if (typeof(body.total_income) !== "number") res.status(400).json({ error: 'property total_income is not a number' })
+            if (typeof(body.total_liabilities) !== "number") res.status(400).json({ error: 'property total_liabilities is not a number' })
+            res.status(200).json({ borrowing: body.total_income * 5 - body.total_liabilities })
+        }
+        else res.status(405).json({ error: 'method not allowed' })
+    } catch (err) {
+        res.status(500).json({ error: 'server error' })
+    }
 }
