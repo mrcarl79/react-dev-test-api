9cf6973a05a6d8896f1ab2df43924b9bc28d46e2
Carl Southgate <carl@tella.co.nz>
2022-05-04 18:33:58 +1200

    feat: added deposit and property_price

--- pages/api/test.ts
+++ pages/api/test.ts
@@ -3,6 +3,7 @@ import { NextApiRequest, NextApiResponse } from 'next'
 type Response = {
     error?: string
     borrowing?: number
+    property_price?: number
 }
 
 export default function handler(req: NextApiRequest, res: NextApiResponse<Response>) {
@@ -12,9 +13,12 @@ export default function handler(req: NextApiRequest, res: NextApiResponse<Respon
             const body = JSON.parse(req.body)
             if (!body.total_income) res.status(400).json({ error: 'missing property total_income' })
             if (!body.total_liabilities) res.status(400).json({ error: 'missing property total_liabilities' })
+            if (!body.deposit) res.status(400).json({ error: 'missing property deposit' })
             if (typeof(body.total_income) !== "number") res.status(400).json({ error: 'property total_income is not a number' })
             if (typeof(body.total_liabilities) !== "number") res.status(400).json({ error: 'property total_liabilities is not a number' })
-            res.status(200).json({ borrowing: body.total_income * 5 - body.total_liabilities })
+            if (typeof(body.deposit) !== "number") res.status(400).json({ error: 'property deposit is not a number' })
+            const borrowing = body.total_income * 5 - body.total_liabilities
+            res.status(200).json({ borrowing: borrowing, property_price: borrowing + body.deposit })
         }
         else res.status(405).json({ error: 'method not allowed' })
     } catch (err) {
